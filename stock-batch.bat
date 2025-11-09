485e7b8f4c5bbb5de8dc252113005ccfb4c15a29
nongoben <omyim_rabbit4@hotmail.com>
2025-11-09 21:38:51 +0700

    [Add] file bat

---
+++ stock-batch.bat
@@ -0,0 +1,5 @@
+@echo off
+timeout /t 10 /nobreak >nul
+cd /d "D:\stockapp\publish"
+start "" "D:\stockapp\publish\StockManagementSystem.exe"
+exit
---
+++ stock-batch.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "D:\stockapp\publish\start-stock-app.bat", 0, False
